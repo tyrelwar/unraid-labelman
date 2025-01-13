52ae87f7db8bd6079c3d5f3c0bd04286ebaf53b0
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-01-13 03:22:20 +0000

    feat: table on main page

--- src/usr/local/emhttp/plugins/labelman/include/Pages/Main.php
+++ src/usr/local/emhttp/plugins/labelman/include/Pages/Main.php
@@ -25,19 +25,61 @@ require_once "{$docroot}/plugins/labelman/include/common.php";
 $dockerOut = Utils::run_command('docker container ls --format="{{.Names}}" --filter "label=net.unraid.docker.managed=dockerman"');
 sort($dockerOut, SORT_STRING | SORT_FLAG_CASE);
 ?>
+<script src="/webGui/javascript/jquery.tablesorter.widgets.js"></script>
+<link type="text/css" rel="stylesheet" href="/plugins/labelman/style.css">
 
 <h3>Label Manager</h3>
 
 Please select the container you would like to manage:
 
-<ul>
+<table id='statusTable' class="unraid statusTable tablesorter">
+    <thead>
+        <tr>
+            <th>Container</th>
+            <th class="filter-select filter-match">TSDProxy Enabled</th>
+            <th class="filter-false">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach ($dockerOut as $c) {
+                $configFile = realpath("/boot/config/plugins/dockerMan/templates-user/my-{$c}.xml");
+                if ( ! $configFile || ! str_starts_with($configFile, "/boot/config/plugins/dockerMan/templates-user/my-")) {
+                    continue;
+                }
+                $container = new Container($configFile);
 
-<?php
+                $enabled = $container->TSDProxy->enable ? "Yes" : "No";
 
-foreach ($dockerOut as $c) {
-    $containerURL = urlencode($c);
-    echo("<li><a href='/Settings/Labelman?container={$containerURL}'>{$c}</a></li>");
-}
+                $containerURL = urlencode($c);
+                echo("<tr><td>{$c}</td><td>{$enabled}</td><td><a href='/Settings/Labelman?container={$containerURL}'>Edit</a></td></tr>");
+            }
 ?>
+    </tbody>
+</table>
+<button type="button" class="reset">Reset Filters</button>
 
-</ul>
+<script>
+$(function() {
+    $('#statusTable').tablesorter({
+      widthFixed : true,
+      sortList: [[0,0]],
+      sortAppend: [[0,0]],
+      widgets: ['stickyHeaders','filter','zebra'],
+      widgetOptions: {
+        // on black and white, offset is height of #menu
+        // on azure and gray, offset is height of #header
+        stickyHeaders_offset: ($('#menu').height() < 50) ? $('#menu').height() : $('#header').height(),
+        filter_columnFilters: true,
+        filter_reset: '.reset',
+        filter_liveSearch: true,
+
+        zebra: ["normal-row","alt-row"]
+      }
+    });
+});
+
+</script>
+
+
+
